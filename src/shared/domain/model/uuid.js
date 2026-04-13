6f52476a27c4efb8202bbf5c1b965cc62e8d174d
Aldair Ramos <u20201f051@upc.edu.pe>
2026-04-13 10:45:03 -0500

    feat (uuid): add module to manage uuid function.

---
+++ src/shared/domain/model/uuid.js
@@ -0,0 +1,8 @@
+import {v4 as uuidv4, validate, validate as uuidValidate} from 'uuid';
+export function generateUuid() {
+    return uuidv4();
+}
+
+export function validateUuid(value) {
+    return uuidValidate(value);
+}
